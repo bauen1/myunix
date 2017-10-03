917b2bb2f2e04b3eb0f4b4490e0e742e93d3c8f0
bauen1 <j2468h@gmail.com>
2017-10-03 19:37:53 +0200

    Fixes to the build system

--- kernel/kmain.c
+++ kernel/kmain.c
@@ -5,6 +5,9 @@
 #include <multiboot.h>
 
 void kmain(struct multiboot_info *mbi, uint32_t eax, uintptr_t esp) {
+	if (eax != MULTIBOOT_BOOTLOADER_MAGIC) {
+		halt();
+	}
 	*((uint32_t *)0xb8000) = 0x414f414f;
-	_halt();
+	halt();
 }
