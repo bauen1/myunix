6c7801f9f7298e76fc1870cbe36f8e9743fb53d5
bauen1 <j2468h@gmail.com>
2018-03-03 14:51:24 +0100

    Move gdt definitions to include/gdt.h

--- kernel/gdt.c
+++ kernel/gdt.c
@@ -5,54 +5,9 @@
 #include <string.h>
 #include <tss_flush.h>
 
-// FIXME: move to arch/i686 and arch/x86_64
-
-typedef struct tss {
-	uint32_t link __attribute__((packed));
-	uint32_t esp0 __attribute__((packed));
-	uint32_t ss0 __attribute__((packed));
-	uint32_t esp1 __attribute__((packed));
-	uint32_t ss1 __attribute__((packed));
-	uint32_t esp2 __attribute__((packed));
-	uint32_t ss2 __attribute__((packed));
-	uint32_t cr3 __attribute__((packed));
-	uint32_t eip __attribute__((packed));
-	uint32_t eflags __attribute__((packed));
-	uint32_t eax __attribute__((packed));
-	uint32_t ecx __attribute__((packed));
-	uint32_t ebx __attribute__((packed));
-	uint32_t esp __attribute__((packed));
-	uint32_t ebp __attribute__((packed));
-	uint32_t esi __attribute__((packed));
-	uint32_t edi __attribute__((packed));
-	uint32_t es __attribute__((packed));
-	uint32_t cs __attribute__((packed));
-	uint32_t ss __attribute__((packed));
-	uint32_t ds __attribute__((packed));
-	uint32_t fs __attribute__((packed));
-	uint32_t gs __attribute__((packed));
-	uint32_t ldt __attribute__((packed));
-	uint16_t trap __attribute__((packed));
-	uint16_t iopb_offset __attribute__((packed));
-} __attribute__((packed)) tss_t;
-
-typedef struct gdt_entry {
-	uint16_t limit_low __attribute__((packed));
-	uint16_t base_low __attribute__((packed));
-	uint8_t base_middle __attribute__((packed));
-	uint8_t access __attribute__((packed));
-	uint8_t granularity __attribute__((packed));
-	uint8_t base_high __attribute__((packed));
-} __attribute__((packed)) gdt_entry_t;
-
-typedef struct {
-	uint16_t limit __attribute__((packed));
-	uintptr_t base __attribute__((packed));
-} __attribute__((packed)) gdt_pointer_t;
-
-static gdt_pointer_t gdt_pointer;
-static gdt_entry_t gdt[6];
-static tss_t tss;
+gdt_pointer_t gdt_pointer;
+__attribute__((aligned(4096))) gdt_entry_t gdt[6];
+__attribute__((aligned(4096))) tss_t tss;
 
 static void gdt_set_gate(uint8_t i, uint32_t base, uint32_t limit, uint8_t access, uint8_t granularity) {
 	gdt[i].limit_low = (uint16_t)(limit & 0xFFFF);
--- kernel/include/gdt.h
+++ kernel/include/gdt.h
@@ -2,6 +2,55 @@
 #define GDT_H 1
 
 #include <stddef.h>
+#include <stdint.h>
+
+// FIXME: move to arch/i686 and arch/x86_64
+typedef struct tss {
+	uint32_t link __attribute__((packed));
+	uint32_t esp0 __attribute__((packed));
+	uint32_t ss0 __attribute__((packed));
+	uint32_t esp1 __attribute__((packed));
+	uint32_t ss1 __attribute__((packed));
+	uint32_t esp2 __attribute__((packed));
+	uint32_t ss2 __attribute__((packed));
+	uint32_t cr3 __attribute__((packed));
+	uint32_t eip __attribute__((packed));
+	uint32_t eflags __attribute__((packed));
+	uint32_t eax __attribute__((packed));
+	uint32_t ecx __attribute__((packed));
+	uint32_t ebx __attribute__((packed));
+	uint32_t esp __attribute__((packed));
+	uint32_t ebp __attribute__((packed));
+	uint32_t esi __attribute__((packed));
+	uint32_t edi __attribute__((packed));
+	uint32_t es __attribute__((packed));
+	uint32_t cs __attribute__((packed));
+	uint32_t ss __attribute__((packed));
+	uint32_t ds __attribute__((packed));
+	uint32_t fs __attribute__((packed));
+	uint32_t gs __attribute__((packed));
+	uint32_t ldt __attribute__((packed));
+	uint16_t trap __attribute__((packed));
+	uint16_t iopb_offset __attribute__((packed));
+} __attribute__((packed)) tss_t;
+
+typedef struct gdt_entry {
+	uint16_t limit_low __attribute__((packed));
+	uint16_t base_low __attribute__((packed));
+	uint8_t base_middle __attribute__((packed));
+	uint8_t access __attribute__((packed));
+	uint8_t granularity __attribute__((packed));
+	uint8_t base_high __attribute__((packed));
+} __attribute__((packed)) gdt_entry_t;
+
+typedef struct {
+	uint16_t limit __attribute__((packed));
+	uintptr_t base __attribute__((packed));
+} __attribute__((packed)) gdt_pointer_t;
+
+extern gdt_pointer_t gdt_pointer;
+extern gdt_entry_t gdt[6];
+extern tss_t tss;
 
 void gdt_init();
 
