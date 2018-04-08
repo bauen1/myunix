15d9b30e22f0fe221af1cf52e7fb1836408f8b00
bauen1 <j2468h@gmail.com>
2018-04-08 11:51:21 +0200

    Added strcpy and memcmp

--- kernel/include/string.h
+++ kernel/include/string.h
@@ -9,5 +9,8 @@ size_t strlen(const char *str);
 void *memset(void *dest, int c, size_t len);
 
 void *memcpy(void *dest, const void *src, size_t n);
+char *strcpy(char *dest, const char *src);
+
+int memcmp(const void *v1, const void *v2, size_t len);
 
 #endif
--- kernel/string.c
+++ kernel/string.c
@@ -11,6 +11,31 @@ size_t strlen(const char *str) {
 	return len;
 }
 
+char *strcpy(char *dest, const char *src) {
+	assert(dest != NULL);
+	assert(src != NULL);
+
+	size_t i = 0;
+	while (src[i] != 0) {
+		dest[i] = src[i];
+		i++;
+	}
+	return dest;
+}
+
+int memcmp(const void *v1, const void *v2, size_t len) {
+	assert(v1 != NULL);
+	assert(v2 != NULL);
+	char *l = (char *)v1;
+	char *r = (char *)v2;
+	for (size_t i = 0; i < len; i++) {
+		if (l[i] != r[i]) {
+			return l[i] - r[i];
+		}
+	}
+	return 0;
+}
+
 void *memset(void *dest, int c, size_t len) {
 	assert(dest != NULL);
 	assert(len != 0);
