c7491b220ac444283ff3908fca43182411ca072c
bauen1 <j2468h@gmail.com>
2018-05-22 18:00:40 +0200

    Added missing init.c

---
+++ init.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdint.h>
+
+int main(int argc, char *argv[]) {
+	setvbuf(stdin, NULL, _IONBF, 0);
+	printf("hello from /init!\n");
+	printf("argc: %i\n", argc);
+	for (int i = 0; i < argc; i++) {
+		printf("argv[%i]: '%s'\n", i, argv[i]);
+	}
+	while(1) {
+		putchar(getchar());
+	}
+	return EXIT_FAILURE;
+}
