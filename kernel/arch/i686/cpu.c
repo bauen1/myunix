2cd1897c54fd2f26dfe2f0f144c19e63f4f0a7fa
bauen1 <j2468h@gmail.com>
2018-10-14 22:46:41 +0200

    kernel/arch/i686/cpu.c: fix dump_registers alignment

--- kernel/arch/i686/cpu.c
+++ kernel/arch/i686/cpu.c
@@ -44,10 +44,10 @@ inline __attribute__((noreturn)) void halt() {
 }
 
 void dump_regs(registers_t *regs) {
-	printf("========\n");
-	printf("es:  0x%8x  fs: 0x%8x  es: 0x%8x  ds: 0x%8x  cs: 0x%8x  ss: 0x%8x\n", regs->gs, regs->fs, regs->es, regs->ds, regs->cs, regs->ss);
-	printf("edi: 0x%8x esi: 0x%8x ebp: 0x%8x ebx: 0x%8x edx: 0x%8x ecx: 0x%8x eax: 0x%8x\n", regs->edi, regs->esi, regs->ebp, regs->ebx, regs->edx, regs->ecx, regs->eax);
+	printf("===================================================================================================================\n");
+	printf("es:      0x%8x  fs: 0x%8x  es: 0x%8x  ds: 0x%8x  cs: 0x%8x  ss: 0x%8x\n", regs->gs, regs->fs, regs->es, regs->ds, regs->cs, regs->ss);
+	printf("edi:     0x%8x esi: 0x%8x ebp: 0x%8x ebx: 0x%8x edx: 0x%8x ecx: 0x%8x eax: 0x%8x\n", regs->edi, regs->esi, regs->ebp, regs->ebx, regs->edx, regs->ecx, regs->eax);
 	printf("isr_num: 0x%8x err_code: 0x%8x\n", regs->isr_num, regs->err_code);
-	printf("eip: 0x%8x eflags: 0x%8x esp: 0x%8x\n", regs->eip, regs->eflags, regs->esp);
-	printf("========\n");
+	printf("eip:     0x%8x eflags: 0x%8x esp: 0x%8x\n", regs->eip, regs->eflags, regs->esp);
+	printf("===================================================================================================================\n");
 }
